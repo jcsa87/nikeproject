0e22a330d91932de306c7f8d652f98e2ea9d57bd
MAximo-21 <maxiximo21@gmail.com>
2025-04-22 20:07:27 -0300

    22/4

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -9,7 +9,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- CSS externo -->
-  <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href="assets/css/homestyle.css">
 </head>
 
 <body>
