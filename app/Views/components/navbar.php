ab879cfd96131aa91463e3ee5780662d46a07690
bostonlowk <juancruzsenicenacosta@gmail.com>
2025-04-25 13:20:05 -0300

    fixeo href en la navbar

--- app/Views/components/navbar.php
+++ app/Views/components/navbar.php
@@ -1,29 +1,28 @@
-<!-- BARRA DE NAVEGACIÓN -->
 <link rel="stylesheet" href="assets/css/navbarstyle.css">
 
 <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img src="assets/img/logitosinplb.jpg" alt="Logo">
-      </a>
+        <a class="navbar-brand" href="<?= base_url('/'); ?>">
+            <img src="<?= base_url('assets/img/logitosinplb.jpg'); ?>" alt="Logo">
+        </a>
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item"><a class="nav-link" href="/nikeproyect/">Inicio</a></li>
-          <li class="nav-item"><a class="nav-link" href="/nikeproyect/about" ?>Nosotros</a></li>
-          <li class="nav-item"><a class="nav-link" href="/nikeproyect/comercialization" ?>Comercialización</a></li>
-          <li class="nav-item"><a class="nav-link" href="/nikeproyect/contact" ?>Contacto</a></li>
-          <li class="nav-item"><a class="nav-link" href="/nikeproyect/terms-uses" ?>Términos y Condiciones</a></li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar">
-          <button class="btn btn-buscar" type="submit">Buscar</button>
-        </form>
-      </div>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link" href="<?= base_url('/'); ?>">Inicio</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?= base_url('/about'); ?>">Nosotros</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?= base_url('/comercialization'); ?>">Comercialización</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?= base_url('/contact'); ?>">Contacto</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?= base_url('/terms-uses'); ?>">Términos y Condiciones</a></li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar">
+                <button class="btn btn-buscar" type="submit">Buscar</button>
+            </form>
+        </div>
     </div>
 </nav>
