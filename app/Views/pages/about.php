5431a535f28d73413bba8d525a9caebb212994c6
bostonlowk <juancruzsenicenacosta@gmail.com>
2025-04-26 20:22:12 -0300

    implementación about

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -14,11 +14,20 @@
     </h1>
 </div>
 
-<div class="object-fit-contain">
+<div class="container my-4">
     <video src="assets/img/about/NIKE_COMPANY_loop_032323.mp4" autoplay muted loop playsinline>
         Tu navegador no soporta el video.
         </video>
 </div>
+
+<div class="quienes-somos">
+    <h1>¿Quiénes somos?</h1>
+    <img  class="container-fluid" src="assets/img/about/logos-brands.webp">
+
+    <h3 class="container-fluid">NIKE, Inc. es un equipo formado por las marcas Nike, Jordan y Converse impulsadas por el propósito compartido de dejar un impacto duradero.</h3>
+</div>
+
+
 
 
 <?= $this->endSection() ?>
